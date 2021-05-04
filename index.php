6267b51727357a70296a7b125aae2cd233f3b1ce
ADM - Fabrice DUJARDIN <adm.fdujardin@ape.local>
2021-05-05 10:41:06 +1100

    connection database

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
-<?php require 'classes/posts.php';?>
+<?php require 'classes/database.php';?>
 <?php include("model/header.php");?>
 <?php include('model/parts/navbar.php'); ?>
-
 <?php include("model/main.php");?>
-
 <?php require 'model/footer.php';?>
--- views/login.php
+++ views/login.php
@@ -1,6 +1,6 @@
 <?php
 include("../model/header.php");
-require_once '../dbconfig.php';
+require_once '../classes/database.php';
 
 session_start();
 
@@ -11,8 +11,8 @@ if(isset($_SESSION["user_login"]))	//check condition user login not direct back 
 
 if(isset($_REQUEST['valider']))	//button name is "btn_login"
 {
-	$email		=strip_tags($_REQUEST["txt_email"]);	//textbox name "txt_username_email"
-	$password	=strip_tags($_REQUEST["txt_password"]);		//textbox name "txt_password"
+	$email		= strip_tags($_REQUEST["email"]);	//textbox name "txt_username_email"
+	$password	= strip_tags($_REQUEST["passwords"]);		//textbox name "txt_password"
 	if(empty($email)){
 		$errorMsg[]="please enter your email";	//check "email" textbox not empty
 	}
@@ -23,14 +23,15 @@ if(isset($_REQUEST['valider']))	//button name is "btn_login"
 	{
 		try
 		{
-			$select_stmt=$conn->prepare("SELECT * FROM Users WHERE email=:uemail"); //sql select query
+			$select_stmt=$conn->prepare("SELECT * FROM agents WHERE email=:uemail"); //sql select query
 			$select_stmt->execute(array(':uemail'=>$email));	//execute query with bind parameter
 			$row=$select_stmt->fetch(PDO::FETCH_ASSOC);			
 			if($select_stmt->rowCount() > 0)	//check condition database record greater zero after continue
 			{
 				if($email==$row["email"]) //check condition user taypable "email" is match from database "email" after continue
 				{
-					if(password_verify($password, $row["password"])) //check condition user taypable "password" is match from database "password" using password_verify() after continue
+                    echo "ok";
+					if($password==$row["passwords"]) //check condition user taypable "password" is match from database "password" using password_verify() after continue
 					{
 						$_SESSION["user_login"] = $row["id"];	//session name is "user_login"
                         $_SESSION["loggedIn"] = true;
@@ -59,56 +60,52 @@ if(isset($_REQUEST['valider']))	//button name is "btn_login"
 	}
 }
 ?>
-<div class="container d-flex mt-4 h-mini-90">
-    <div class="col-lg-12">
-        <?php
+<body>
+    <div class="container d-flex mt-4 h-mini-90">
+        <div class="col-lg-12">
+            <?php
             if(isset($errorMsg))
             {
                 foreach($errorMsg as $error)
                 {
                 ?>
-        <div class="alert alert-danger">
-            <strong><?php echo $error; ?></strong>
-        </div>
-        <?php
+                    <div class="alert alert-danger">
+                        <strong><?php echo $error; ?></strong>
+                    </div>
+                <?php
                 }
             }
             if(isset($loginMsg))
             {
             ?>
-        <div class="alert alert-success">
-            <strong><?php echo $loginMsg; ?></strong>
-        </div>
-        <?php
+                <div class="alert alert-success">
+                    <strong><?php echo $loginMsg; ?></strong>
+                </div>
+            <?php
             }
             ?>
-        <form>
-    <div class="row justify-content-md-center">
-        <div class="col-4 text-center">
-            <div class="title">
-                <h1>Authentification</h1>
-            </div>
+            <form action="" method="post" name="fo">
+                <!-- <div class="erreur"><?php echo $erreur ?></div> -->                
+                <div class="text-center col-12 mt-1">
+                    <img  src="../public/images/man-logo.svg" alt="" width="72" height="72">
+                </div>
+                <div class="text-center">
+                <h1><strong>PROJECT BLOG</strong></a></h1>
+                </div>
+                <h2 class="h3 mb-3 font-weight-normal text-center">Veuillez vous connecter<hr></h2>
+                <div class="form-group">
+                    <label for="loginEmail" class="pb-1"><strong>Email :</strong></label>
+                    <input type="text" class="form-control" id="loginEmail"  placeholder="Enter votre email" name="email">
+                </div>
+                <div class="form-group pt-2">
+                    <label for="Passwordid" class="pb-1"><strong>Password :</strong></label>
+                    <input type="password" class="form-control" id="Passwordid" placeholder="Enter votre mots de pass" name="passwords">
+                </div>
+                <div class="text-center  mb-3">
+                <button class="btn btn-lg btn-facebook-b btn-block mt-4 text-center" type="submit" name="valider" value="S'authentifier">S'identifier</button>
+                </div> <span><a href="../index.php" class="text-decoration-none">Retour à l'accueil</a></span><span  class="ms-4 "><a href="registration.php" class="text-decoration-none">Vous n'avez pas de compte ? Créez vous un compte</a> </span>
+            </form>
         </div>
     </div>
-    <div class="row bg-light">
-        <div class="row justify-content-md-center p-5">
-            <div class="col-md-3">
-                <label for="exampleInputEmail" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
-            </div>
-            <div class="col-md-3">
-                <label for="inputPassword" class="form-label">Mot de Passe</label>
-                <input type="password" class="form-control" id="inputPassword">
-            </div>
-        </div>
-        <div class="row justify-content-md-center p-5 pt-0">
-            <div class="col-2 text-center">
-                <button class="btn btn-primary" type="submit">Connexion</button>
-            </div>
-        </div>
-    </div>
-</form>
-
-
-    </div>
-</div>
+</body>
+<?php include("../model/footer.php");?>
