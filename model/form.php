3544dee3dfcb75dad2d8becfdd78c5c443d51a28
hnesaiqe <69833471+hnesaiqe@users.noreply.github.com>
2021-05-06 14:27:10 +1100

    Alignement CSS

--- model/form.php
+++ model/form.php
@@ -38,9 +38,9 @@ if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == false):
 
       <div class="col-md-12 d-flex text-center my-5">
         <div class="col-md-6">
-          <div class="col-md-12 d-flex">
+          <div class="col-md-12 d-flex align-items-center">
             <div class="col-md-4">
-              <label for="inputLastName" class="form-label">DATE DE DÉBUT</label>
+              <label for="inputLastName" class="form-label m-0">DATE DE DÉBUT</label>
             </div>
             <div class="col-md-8">
               <input type="date" id="dayNow" class="form-control" name="test" value="<?php echo date('Y-m-d'); ?>">
@@ -48,9 +48,9 @@ if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == false):
           </div>
         </div>
         <div class="col-md-6">
-          <div class="col-md-12 d-flex mx-auto">
+          <div class="col-md-12 d-flex align-items-center ">
             <div class="col-md-4 ">
-              <label for="inputLastName" class="form-label">DATE DE FIN</label>
+              <label for="inputLastName" class="form-label m-0">DATE DE FIN</label>
             </div>
             <div class="col-md-8">
               <input type="date" class="form-control" name=" ">
@@ -77,53 +77,6 @@ if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == false):
         </div>
       </div>
     </form>
-
-
-    <!-- ************Table de récap***************** -->
-
-
-    <div class="pole col-3 border border-1 border-dark mt-5 mb-1 text-center" style="background-color:#CFE2FF">
-      <h3>Agents Absents</h3>
-    </div>
-    <table class="table table-primary table-striped">
-
-
-      <thead>
-        <tr>
-          <th scope="col">Pôle</th>
-          <th scope="col">Nom</th>
-          <th scope="col">Prénom</th>
-          <th scope="col">Date de Début</th>
-          <th scope="col">Date de Fin</th>
-          <th scope="col">Motifs</th>
-          <th scope="col">Statut</th>
-          <th scope="col">Action</th>
-
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">COM</th>
-          <td>Otto</td>
-          <td>Mark</td>
-          <td>05/05/2021</td>
-          <td>07/05/2021</td>
-          <td>CP</td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <th scope="row">COM</th>
-          <td>Otto</td>
-          <td>Mark</td>
-          <td>05/05/2021</td>
-          <td>07/05/2021</td>
-          <td>CP</td>
-          <td></td>
-          <td></td>
-        </tr>
-    </table>
-
 <!-- ************Table de récap***************** -->
 <?php 
                     try {                              
