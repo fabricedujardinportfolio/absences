378210526d6adfa61e8fd68ca099a17199f28492
ADM - Fabrice DUJARDIN <adm.fdujardin@ape.local>
2021-05-28 13:47:13 +1100

    update

--- model/form.php
+++ model/form.php
@@ -20,7 +20,7 @@ if(isset($_POST['button-absence']))
         $dateEndUpdate = date('Y-m-d', strtotime($dateEnd));
         $motif_start_id = $_POST['motif_start_id'];
         $motif_end_id = $_POST['motif_end_id'];
-        var_dump($_POST);
+        // var_dump($_POST);
         // var_dump($dateEndUpdate);
         // $motif = $_POST['motif'];
         // $motif_int = (int)$motif;
