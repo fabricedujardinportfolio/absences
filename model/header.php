b4f22182295c5e305e6d09795302e208352eee55
ADM - Fabrice DUJARDIN <adm.fdujardin@ape.local>
2021-05-31 11:11:21 +1100

    rajout d'un favicon url du header rendu dynamique

--- model/header.php
+++ model/header.php
@@ -2,6 +2,7 @@
 <html lang="fr">
 <head>
 <?php session_start();?>
+    <link rel="icon" type="image/ico" href="giep-nc.ico" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- model/parts/navbar.php
+++ model/parts/navbar.php
@@ -1,6 +1,6 @@
 
 <?php
-  $root = "http://192.168.40.77/applications/absence/";
+  $root = "http://absences/";
   $login_path = $root . "views/login.php";
   $logout_path = $root . "views/logout.php";
   $register_path = $root . "views/registration.php";
@@ -31,21 +31,23 @@ if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] == false):
     else: 
         if ($_SESSION["user_pole"] === "COMMUNICATION / DOCUMENTATION​") {            
             echo"
-            <h6 class='mt-3 ms-2'>ADMIN COMMUNICATION / DOCUMENTATION​</h6>
+            <div class='container-fluid d-flex mt-2 align-self-center'>
+            <h6 class='mt-3 ms-2 mt-auto'>ADMIN PANEL COMMUNICATION / DOCUMENTATION​ :</h6>
             <ul class='nav nav-pills ms-2'>
-                <li class='nav-item'> <a href='http://192.168.40.77/applications/absence/views/add_agents.php' style='background-color:#2e4f9b;' target='_blank' class='nav-link active'> RAJOUTER UN AGENT</a></li>
-            </ul>";
+                <li class='nav-item'> <a href='http://absences/views/add_agents.php' style='background-color:#2e4f9b;' target='_blank' class='nav-link active'> RAJOUTER UN AGENT</a></li>
+            </ul>
+            </div>";
             }
 ?>
 
 <div class="container-fluid">
-<header class="col-md-12 m-auto d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+<header class="col-md-12 m-auto d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom d-flex">
     <a href="<?php echo $root . "index.php" ?>"
         class="col-md-6 col-12 d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none flex-wrap justify-content-center">
         <span class="fs-4">GIEP-NC ABSENCES</span>
     </a>
     <ul class="nav nav-pills">
-        <li class="nav-item" ><a href="http://intranet_dev/liste-des-agents-absents-du-giep-nc/" class="nav-link active" style="background-color:#2e4f9b;" target="_blank">Intranet</a></li>
+        <li class="nav-item" ><a href="http://intranet/agents-absents/" class="nav-link active" style="background-color:#2e4f9b;" target="_blank">Intranet</a></li>
         <li class="nav-item"><a href="<?php echo $logout_path; ?>" class="nav-link text-uppercase" style="color:#2e4f9b;">Déconnexion</a></li>
     </ul>
 </header>
